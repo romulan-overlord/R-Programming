189a37a016203bfeede0d5a4d6930052faecabd1
romulan-overlord <riturajpradhan911@gmail.com>
2024-02-23 13:31:29 +0530

    eigenvalues and eigenvectors

---
+++ 23908_MDSC-202(P)_Eigen.R
@@ -0,0 +1,17 @@
+library(geometry)
+install.packages(geometry)
+M = matrix(c(1,2,3,4,5,6,7,8,9), nrow = 3, ncol = 3)
+
+eigen_data = eigen(M)
+eigenvalues = eigen_data$values
+eigenvectors = eigen_data$vectors
+norm_vectors = eigenvectors
+
+for(col in 1:3){
+  # norm_vectors[,col] = norm_vectors[,col]/(dot(norm_vectors[,col], norm_vectors[,col], d=NULL))
+  dot = 0
+  for (x in norm_vectors[,col]){
+    dot = dot + x*x
+  }
+  norm_vectors[,col] = norm_vectors[,col]/sqrt(dot)
+}
